9acad702c735f3631c487835e8bc1fff97ecc5e5
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-06-04 17:32:45 +0700

    [wip] bai 2

--- BaiTap01/ThanhNX/BaiTap1.playground/Contents.swift
+++ BaiTap01/ThanhNX/BaiTap1.playground/Contents.swift
@@ -8,3 +8,19 @@ func acreageAndVolumeGlobular(_ radius: Double) -> (Double, Double) {
 let display = acreageAndVolumeGlobular(15)
 print("Acreage and Volume of Globular Radius = 15")
 print("Acreage is: \(display.0) and Volume is: \(display.1)")
+
+//Bai 2:
+func ptBac2(_ a: Float,_ b: Float, _ c: Float) -> (Float, Float){
+    let delta: Float = powf(b, 2) - 4*a*c
+    if delta < 0 {
+        return (0, 0)
+    } else if delta == 0 {
+        return (0 , -b / (2*a))
+    }else{
+        return ((-b + sqrtf(delta) / 2*a),((-b - sqrtf(delta) / 2*a)))
+    }
+}
+
+let bai2 = ptBac2(3, 2, -9)
+
+print("Nghiệm phương trình bậc 2")
