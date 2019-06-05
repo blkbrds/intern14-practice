312991aa079173d4b1136e1d7f6e40d00f46d153
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-06-05 09:24:28 +0700

    Duong.Nguyen fix comments from mentor

--- BaiTap01/DuongNT/BasicSwift.playground/Contents.swift
+++ BaiTap01/DuongNT/BasicSwift.playground/Contents.swift
@@ -1,14 +1,14 @@
 import UIKit
 
-//Bài tập 1:
+// Bài tập 1:
 // use "-" instead of args label in func
 func calculateGlobular(_ r : Double) -> (dienTich: Double, theTich: Double){
-    return (4*Double.pi*pow(r, 2),(4*Double.pi*pow(r, 3))/3)
+    return (4 * Double.pi * pow(r, 2),(4 * Double.pi * pow(r, 3)) / 3)
 }
-let hinhcau = calculateGlobular(4)
+let hinhCau = calculateGlobular(4)
 print("""
     Diện tích và thể tích hình cầu có bán kính là 4!
-    Diện tích: \(hinhcau.dienTich)
-    Thể tích: \(hinhcau.theTich)
+    Diện tích: \(hinhCau.dienTich)
+    Thể tích: \(hinhCau.theTich)
     """)
 
