497155128fefb70f6edd5fc5e9d804e34c01b05e
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-06-05 11:06:38 +0700

    update bai 2

--- BaiTap01/ThanhNX/BaiTap1.playground/Contents.swift
+++ BaiTap01/ThanhNX/BaiTap1.playground/Contents.swift
@@ -8,3 +8,37 @@ func acreageAndVolumeGlobular(_ radius: Double) -> (Double, Double) {
 let display = acreageAndVolumeGlobular(15)
 print("Acreage and Volume of Globular Radius = 15")
 print("Acreage is: \(display.0) and Volume is: \(display.1)")
+
+//Bai 2: ax2 + bx + c = 0
+enum dapAn {
+    case VoNghiem
+    case CoMotNghiem (Float)
+    case Co2Nghiem (Float, Float)
+}
+
+func ptBac2(_ a: Float,_ b: Float, _ c: Float) -> dapAn {
+    let delta: Float = powf(b, 2) - 4 * a * c
+    if a == 0 {
+        if b == 0 {
+            return .VoNghiem
+        }else{
+            return .CoMotNghiem(-c / b)
+        }
+    }else{
+        if delta < 0 {
+            return .VoNghiem
+        } else if delta == 0 {
+            return .CoMotNghiem(-b / (2 * a))
+        }else{
+            return .Co2Nghiem (((-b + sqrtf(delta)) / (2 * a)) , ((-b - sqrtf(delta)) / (2 * a)))
+        }
+    }
+}
+
+print("Nghiệm phương trình bậc 2")
+print(ptBac2(3, 8, 5))
+
+//Bai 3: a1x + b1y = c1
+//       a2x + b2y = c2
+
+
