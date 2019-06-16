503dad1f4d1b87537a7431ed4bd4c181f3ddaec3
Vien Nguyen T. L <vien.nguyen2@asiantech.vn>
2019-06-17 01:51:12 +0700

    Bổ sung 6 + 8

--- BaiTap02/VienNTL/BaiTap02.playground/Contents.swift
+++ BaiTap02/VienNTL/BaiTap02.playground/Contents.swift
@@ -98,7 +98,6 @@ if let phanSo = pSo, let phanSo2 = pSo2 {
         print("\n .Division: \(phanSo.inPS()) / \(phanSo2.inPS()) = \(chiaPS.inPS())")
     }
 }
-
 // Bài 2
 class DaGiac {
     private(set) var arrayEdge = [Double]()
@@ -137,7 +136,6 @@ var dg = DaGiac(3, arr)
 // In các giá trị của mảng
 dg.output()
 print("The polygon circumference is: \(dg.chuVi(3, arr))")
-
 // Bài 3:
 class TamGiac: DaGiac {
     private(set) var arrayEdgeTriangle = [Double]()
@@ -150,19 +148,19 @@ class TamGiac: DaGiac {
     override func chuVi(_ quaEdge: Int, _ arrEdge: [Double]) -> Double {
         var cv: Double = 0
         for index in arrEdge {
-                cv += index
+            cv += index
         }
         return cv
     }
 
     func DienTich() -> Double {
-        var dt: Double = 0
-        let cv: Double = chuVi(3, arrayEdgeTriangle)
-        for index in 0...1  {
-            let tempDt: Double = cv * (cv - arrayEdgeTriangle[index]) * (cv - arrayEdgeTriangle[index + 1]) * (cv - arrayEdgeTriangle[index + 2])
-            dt = sqrt(tempDt)
+        var dienTich: Double = 0
+        let chuVi: Double = chuVi(3, arrayEdgeTriangle)
+        for  in 0...1  {
+            let tempDienTich: Double = chuVi * (chuVi - arrayEdgeTriangle[index]) * (chuVi - arrayEdgeTriangle[index + 1]) * (chuVi - arrayEdgeTriangle[index + 2])
+            dienTich = sqrt(tempDt)
         }
-        return dt
+        return dienTich
     }
 }
 // Bài 4 In n Tam Gíac
@@ -177,7 +175,7 @@ class NewTriangle {
         self.c = c
     }
 
-    func checkPytago() -> (checkBool: Bool, arr: [Double]){
+    func checkPytago() -> (checkBool: Bool, arr: [Double]) {
         if a  == sqrt(pow(b, 2) + pow(c, 2)) || b == sqrt(pow(a, 2) + pow(c, 2)) || c == sqrt(pow(a, 2) + pow(c, 2)) {
             return (true, [a, b, c])
         }
@@ -200,14 +198,17 @@ class PrintNTRiangle {
         }
     }
 }
-
 // Bài 5:
 class CStack {
-    private(set) var s rray = [Int]()
+    private(set) var stackArray = [Int]()
     private(set) var max = 5
 
-    func isEmpty() -> Bool {
-        return  stackArray.isEmpty
+    func isEmpty() -> Bool{
+        if stackArray.isEmpty {
+            return true
+        } else {
+            return false
+        }
     }
 
     func isFull() -> Bool {
@@ -226,14 +227,218 @@ class CStack {
         return stackArray.popLast()
     }
 }
+// Bài 6:
+class HinhVe {
+    private(set) var chieuDai: Double = 0.0
+    private(set) var chieuRong: Double = 0.0
+    private(set) var pi: Double = Double.pi
 
+    init(_ dai: Double, _ rong: Double) {
+        chieuDai = dai
+        chieuRong = rong
+    }
+    func ouput() {
+        print(" ")
+    }
+    func chuVi() -> Double {
+        return chieuDai
+    }
+    func dienTich() -> Double {
+        return chieuRong
+    }
+    func theTich() -> Double {
+        return 0.0
+    }
+}
+class HaiChieu: HinhVe {
+    override func chuVi() -> Double {
+        return chieuRong + chieuRong
+    }
+    override func dienTich() -> Double {
+        return chieuRong * chieuDai
+    }
+    override func ouput() {
+        print("Hinh Hai Chieu")
+    }
+}
+class Vuong: HaiChieu {
+    override func chuVi() -> Double {
+        return chieuDai * 4
+    }
+    override func dienTich() -> Double {
+        return chieuDai * chieuRong
+    }
+}
+class Tron: HaiChieu {
+    private(set) var r: Double
+
+    init(_ dai: Double, _ rong: Double, _ banKinh: Double) {
+        self.r = banKinh
+        super.init(dai, rong)
+    }
+    override func chuVi() -> Double {
+        return 2 * pi * r
+    }
+    override func dienTich() -> Double {
+        return pi * pow(r, 2)
+    }
+}
+class TamGiac: HaiChieu {
+    private(set) var c: Double
+
+    init(_ a: Double, _ b: Double, _ c: Double ) {
+        self.c = c
+        super.init(a, b)
+    }
+    override func chuVi() -> Double {
+         return (chieuDai + chieuRong + c)
+    }
+    override func dienTich() -> Double {
+        return sqrt((chuVi() / 2) * ((chuVi() / 2) - chieuDai) * ((chuVi() / 2) - chieuRong) * ((chuVi() / 2) - c))
+    }
+}
+class BaChieu: HinhVe {
+    override func ouput() {
+        print("Hinh Ba Chieu")
+    }
+}
+class Cau: BaChieu {
+    private(set) var r: Double
+
+    init(_ dai: Double, _ rong: Double, _ banKinh: Double) {
+        self.r = banKinh
+        super.init(dai, rong)
+    }
+    override func dienTich() -> Double {
+        return 4 * pi * pow(r, 2)
+    }
+    override func theTich() -> Double {
+        return 4 / 3 * pi * pow(r, 3)
+    }
+}
+class LapPhuong: BaChieu {
+    override func theTich() -> Double {
+        return chieuDai * chieuDai * chieuDai
+    }
+    override func dienTich() -> Double {
+        return 6 * pow(a, 2)
+    }
+    func dienTichXungQuanh() -> Double {
+        return 4 * pow(a, 2)
+    }
+}
+// Bài 7:
+class HocSinh {
+    private(set) var hoTen = String()
+    private(set) var namSinh = String()
+    private(set) var tongDiem = 0.0
+
+    init(_ hoten: String, _ namSinh: String, tongDiem: Double){
+        self.hoTen = hoten
+        self.namSinh = namSinh
+        self.tongDiem = tongDiem
+    }
+}
+class QuanLiHocSinh {
+    private(set) var hocSinh = [HocSinh]()
+
+    init(_ sinhVien: [HocSinh]) {
+        self.hocSinh = sinhVien
+    }
+    func sort() -> [HocSinh] {
+        var tempArr = hocSinh
+        for index in 0..<tempArr.count {
+            if tempArr[index].tongDiem == tempArr[index + 1].tongDiem {
+                if tempArr[index].namSinh > tempArr[index + 1].namSinh  {
+                    let temp = tempArr[index]
+                    tempArr[index] = tempArr[index + 1]
+                    tempArr[index + 1] = temp
+                }
+            } else {
+                if tempArr[index].tongDiem < tempArr[index + 1].tongDiem {
+                    let temp = tempArr[index]
+                    tempArr[index] = tempArr[index + 1]
+                    tempArr[index + 1] = temp
+                }
+            }
+        }
+        return tempArr
+    }
+    func getChar(_ string: String, atIndex i: Int) -> String {
+        if i < string.count {
+            return String(string[string.index(string.startIndex, offsetBy: i)])
+        }
+        return ""
+    }
+    func inHoaHoTen() -> [HocSinh] {
+        var tempArr = hocSinh
+        for index in 0..<tempArr.count {
+            for index2 in 0..<tempArr[index].hoTen.count {
+                let temp = tempArr[index].hoTen
+                if (getChar(temp, atIndex: index2 - 1) == " ") {
+                    getChar(temp, atIndex: index2).uppercased()
+                }
+            }
+        }
+        return tempArr
+    }
+}
+// Bài 8:
+class Date {
+    private(set) var day: Int
+    private(set) var month: Int
+    private(set) var year: Int
+    
+    init?(_ day: Int, _ month: Int, _ year: Int) {
+        if day > 31 || day < 1 {
+            return nil
+        } else {
+            self.day = day
+        }
+        if month > 12 || month < 1 {
+            return nil
+        } else {
+            self.month = month
+        }
+        if year < 1 {
+            return nil
+        } else {
+            self.year = year
+        }
+    }
+    
+    func daysIn(_ month: Int) -> Int? {
+        if month > 12 || month < 1 {
+            return nil
+        } else {
+            switch month {
+            case 1, 3, 5, 7, 8, 10, 12:
+                return 31
+            case 4, 6, 9, 11:
+                return 30
+            case 2:
+                if year % 4 == 0 {
+                    return 29
+                } else {
+                    return 28
+                }
+            default:
+                return 0
+            }
+        }
+    }
+    
+    func prints() {
+        print("\(day) / \(month) / \(year)")
+    }
+}
 // Bài 9:
 class ArrayDimensional {
     private(set) var array = [Int]()
     init(_ array: [Int]) {
         self.array = array
     }
-    
+
     func findMax(_ arr: [Int]) -> Int {
         var max = 0
         for i in 0..<arr.count {
@@ -252,18 +457,22 @@ class ArrayDimensional {
         }
         return min
     }
-    
-}
 
+}
 // Bài 10
 class IntNumberSum {
-    var quantity = 0
-    func sum(_ quantity: Int) -> Int {
+    var quantity: Int = 0
+    init(_ quantity: Int) {
+        self.quantity = quantity
+    }
+    func sum() -> Int {
         var sum = 0
-        for index in 0...quantity {
+        for index in 1...self.quantity {
             sum += index
         }
         return sum
     }
-}
 
+}
+var qc = IntNumberSum(10)
+print("\(qc.sumA())")
