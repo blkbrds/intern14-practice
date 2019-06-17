8929b77cd6d64b2db1a4c54397b987efaff2e934
VuNM <vu.nguyen2@asiantech.vn>
2019-06-17 10:56:32 +0700

    Bai tap 09

--- Baitap/OOP9.playground/Contents.swift
+++ Baitap/OOP9.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var str = "Hello, playground"
+class Mang1c {
+    var integerArray: [Int] = []
+    init?(_ integerArray: [Int]) {
+        if integerArray.count == 0 {
+            print("The array is empty")
+            return nil
+        } else {
+        self.integerArray = integerArray
+        }
+    }
+    func printArray () {
+        var addArray: [Int] = []
+        for i in integerArray {
+            addArray.append(i)
+        }
+        print(addArray)
+    }
+    func printMinMax() {
+        if let min = integerArray.min(), let max = integerArray.max() {
+            print("The max integer in array is \(max) \nThe min intger in array is \(min)")
+        } else {}
+        
+    }
+
+}
+if let a = Mang1c([]) {
+a.printArray()
+a.printMinMax()
+} else {}
+
