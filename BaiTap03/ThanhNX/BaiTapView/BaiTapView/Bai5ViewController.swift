a09aa4471dae37f44936fc6436af8428a637f7a8
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-06-21 14:23:51 +0700

    Update bai 5 View

--- BaiTap03/ThanhNX/BaiTapView/BaiTapView/Bai5ViewController.swift
+++ BaiTap03/ThanhNX/BaiTapView/BaiTapView/Bai5ViewController.swift
@@ -33,10 +33,31 @@ class Bai5ViewController: UIViewController {
         Charts(month: "Oct", percent: 4.0),
         Charts(month: "Nov", percent: 5.0),
         Charts(month: "Dec", percent: 4.0)  ]
-
+    var unitsSold2: [Charts] = [Charts(month: "", percent: 20),
+                                Charts(month: "", percent: 19),
+                                Charts(month: "", percent: 18),
+                                Charts(month: "", percent: 17),
+                                Charts(month: "", percent: 16),
+                                Charts(month: "", percent: 15),
+                                Charts(month: "", percent: 14),
+                                Charts(month: "", percent: 13),
+                                Charts(month: "", percent: 12),
+                                Charts(month: "", percent: 11),
+                                Charts(month: "", percent: 10),
+                                Charts(month: "", percent: 9),
+                                Charts(month: "", percent: 8),
+                                Charts(month: "", percent: 7),
+                                Charts(month: "", percent: 6),
+                                Charts(month: "", percent: 5),
+                                Charts(month: "", percent: 4),
+                                Charts(month: "", percent: 3),
+                                Charts(month: "", percent: 2),
+                                Charts(month: "", percent: 1),
+                                Charts(month: "", percent: 0)]
+    
     struct Config {
         static let chartWidth = UIScreen.main.bounds.width
-        static let chartHeight = UIScreen.main.bounds.height - 90
+        static let chartHeight = UIScreen.main.bounds.height - (UIScreen.main.bounds.height / 10)
     }
 
     override func viewDidLoad() {
@@ -54,17 +75,36 @@ class Bai5ViewController: UIViewController {
         chartSmall.backgroundColor = .white
         chartBig.addSubview(chartSmall)
         
-//        let whiteView = UIView(frame: CGRect(x: 0, y: 0, width: chartSmall.bounds.width, height: chartSmall.bounds.height))
-//        whiteView.backgroundColor = .white
-//        chartSmall.addSubview(whiteView)
+        let noteView = UIView(frame: CGRect(x: 38, y: chartSmall.bounds.height + 45, width: chartSmall.bounds.width / 4, height: 15))
+        noteView.backgroundColor = .clear
+        
+        let noteUIView = UIView(frame: CGRect(x: 0, y: 0, width: noteView.bounds.width / 7, height: noteView.bounds.height))
+        noteUIView.backgroundColor = .orange
+        noteView.addSubview(noteUIView)
+        
+        let noteLabel = UILabel(frame: CGRect(x: noteView.bounds.width - (noteView.bounds.width / 7 * 6), y: 0, width: noteView.bounds.width - noteUIView.bounds.height, height: noteView.bounds.height))
+        noteLabel.backgroundColor = .clear
+        noteLabel.text = "Units Sold"
+        noteLabel.textAlignment = .center
+        noteLabel.font = UIFont.systemFont(ofSize: 10)
+        noteView.addSubview(noteLabel)
+        chartBig.addSubview(noteView)
         
         let maxColumnHeight = chartSmall.bounds.height - 45
         
         var xColumn: CGFloat = 1
-        var yColumn: CGFloat = 45
         var xMonthName: CGFloat = 38
+        
         for index in 0..<unitsSold.count {
-            let columns = UIView(frame: CGRect(x: xColumn, y: yColumn, width: chartSmall.bounds.width / 13, height: maxColumnHeight * CGFloat(unitsSold[index].percent / 20)))
+            let numbers = UILabel(frame: CGRect(x: xColumn, y: chartSmall.bounds.height - maxColumnHeight * CGFloat(unitsSold[index].percent / 20) - 10, width: chartSmall.bounds.width / 13, height: 10))
+            numbers.backgroundColor = .clear
+            numbers.text = "\(unitsSold[index].percent)"
+            numbers.textColor = .black
+            numbers.textAlignment = .center
+            numbers.font = UIFont.systemFont(ofSize: 10)
+            chartSmall.addSubview(numbers)
+            
+            let columns = UIView(frame: CGRect(x: xColumn, y: chartSmall.bounds.height - maxColumnHeight * CGFloat(unitsSold[index].percent / 20), width: chartSmall.bounds.width / 13, height: maxColumnHeight * CGFloat(unitsSold[index].percent / 20)))
             columns.backgroundColor = .orange
             chartSmall.addSubview(columns)
             xColumn += chartSmall.bounds.width / 12
@@ -75,8 +115,28 @@ class Bai5ViewController: UIViewController {
             monthName.font = UIFont.systemFont(ofSize: 10)
             chartBig.addSubview(monthName)
             xMonthName += chartSmall.bounds.width / 12
+            
         }
         
+        var yNumberOutside: CGFloat = 50
+        for index2 in 0..<unitsSold2.count {
+            let numberOutside = UILabel(frame: CGRect(x: 2, y: yNumberOutside, width: 20, height: 10))
+            numberOutside.text = "\(unitsSold2[index2].percent)"
+            numberOutside.backgroundColor = .clear
+            numberOutside.font = UIFont.systemFont(ofSize: 8)
+            numberOutside.textAlignment = .center
+            chartBig.addSubview(numberOutside)
+            
+            let numberOutside2 = UILabel(frame: CGRect(x: chartBig.bounds.width - 22, y: yNumberOutside, width: 20, height: 10))
+            numberOutside2.text = "\(unitsSold2[index2].percent)"
+            numberOutside2.backgroundColor = .clear
+            numberOutside2.font = UIFont.systemFont(ofSize: 8)
+            numberOutside2.textAlignment = .center
+            chartBig.addSubview(numberOutside2)
+            
+            yNumberOutside += maxColumnHeight * CGFloat(unitsSold[0].percent / 20) / 20
+            
+        }
         return chartBig
     }
 }
