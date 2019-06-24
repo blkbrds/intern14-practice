2ecedfad1bbb66131c775e300f98631540a0f139
Vien Nguyen T. L <vien.nguyen2@asiantech.vn>
2019-06-24 17:37:47 +0700

    Cập nhật bài 5

--- BaiTap03/VienNTL/BaiTapView/BaiTapView/BaiTap05/BaiTap05ViewController.swift
+++ BaiTap03/VienNTL/BaiTapView/BaiTapView/BaiTap05/BaiTap05ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ValueChart {
+class Chart {
     var name = String()
     var value: Float = 0
     
@@ -16,25 +16,31 @@ class ValueChart {
         self.name = name
         self.value = value
     }
+    
+    // Dummy Data for test
+    static func dummyData() -> [Chart] {
+        return [
+            Chart("Jan", 20.0),
+            Chart("Feb", 90.0),
+            Chart("Mar", 16.0),
+            Chart("Apr", 23.0),
+            Chart("May", 2.0),
+            Chart("Jun", 13.0),
+            Chart("Jul", 8.0),
+            Chart("Aug", 9.0),
+            Chart("Sep", 10.0),
+            Chart("Oct", 5.0),
+            Chart("Nov", 7.0),
+            Chart("Dec", 8.0),
+        ]
+    }
 }
 
 class BaiTap05ViewController: UIViewController {
     
-    var valueChart: [ValueChart] = [
-        ValueChart("Jan", 20.0),
-        ValueChart("Feb", 30.0),
-        ValueChart("Mar", 16.0),
-        ValueChart("Apr", 23.0),
-        ValueChart("May", 2.0),
-        ValueChart("Jun", 13.0),
-        ValueChart("Jul", 8.0),
-        ValueChart("Aug", 9.0),
-        ValueChart("Sep", 10.0),
-        ValueChart("Oct", 5.0),
-        ValueChart("Nov", 7.0),
-        ValueChart("Dec", 8.0),
-        ]
-    var containerSuperView = UIView(frame: CGRect(x: 40, y: 40, width: UIScreen.main.bounds.width - 45, height: UIScreen.main.bounds.height - 100))
+    var items: [Chart] = Chart.dummyData()
+    
+    var containerView = UIView(frame: CGRect(x: 40, y: 40, width: UIScreen.main.bounds.width - 45, height: UIScreen.main.bounds.height - 100))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,40 +50,47 @@ class BaiTap05ViewController: UIViewController {
     }
     
     func output() {
-        containerSuperView.backgroundColor = .white
-        containerSuperView.layer.borderWidth = 1
-        containerSuperView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        containerView.backgroundColor = .white
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
 
-        var x = containerSuperView.bounds.origin.x
+        var x = containerView.bounds.origin.x
         
-        for index in 0..<valueChart.count {
-            let frame = CGRect(x: x, y: containerSuperView.bounds.origin.y, width: 22 , height: UIScreen.main.bounds.height - 100)
-            addColumnChart(frame, valueChart[index].name, valueChart[index].value)
+        for index in 0..<items.count {
+            let frame = CGRect(x: x,
+                               y: containerView.bounds.origin.y,
+                               width: 22,
+                               height: UIScreen.main.bounds.height - 100)
+            
+            addColumnChart(frame: frame,
+                           chart: items[index])
+            
             x += 28
         }
         
-        view.addSubview(containerSuperView)
+        view.addSubview(containerView)
     }
     
-    func addColumnChart(_ frame: CGRect, _ name: String, _ value: Float) {
-        let number = UIScreen.main.bounds.height * CGFloat(value / 100)
+    func addColumnChart(frame: CGRect, chart: Chart) {
+        let number = frame.height * CGFloat(chart.value / 100)
+        
         let column = UIView(frame: CGRect(x: frame.origin.x, y: frame.height - number, width: 22, height: number))
         column.backgroundColor = .orange
-        containerSuperView.addSubview(column)
+        containerView.addSubview(column)
         
         let nameLabel = UILabel(frame: CGRect(x: frame.origin.x, y: frame.height + 8, width: 20, height: 20))
-        nameLabel.text = name
+        nameLabel.text = chart.name
         nameLabel.textColor = .black
         nameLabel.textAlignment = .center
         nameLabel.font = UIFont.systemFont(ofSize: 10)
-        containerSuperView.addSubview(nameLabel)
+        containerView.addSubview(nameLabel)
         
         let valueLabel = UILabel(frame: CGRect(x: frame.origin.x, y: frame.height - number - 15, width: 20, height: 20))
-        valueLabel.text = "\(value)"
+        valueLabel.text = "\(chart.value)"
         valueLabel.textColor = .black
         valueLabel.textAlignment = .center
         valueLabel.font = UIFont.systemFont(ofSize: 8)
-        containerSuperView.addSubview(valueLabel)
+        containerView.addSubview(valueLabel)
 
     }
 }
