cc5dbf60e3ce852271e6e1605959af64b1ebfd70
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-06-25 15:38:55 +0700

    update bai 3 IBOutlet & IBAction

--- BaiTap04/ThanhNX/IBOutletAndIBAction/IBOutletAndIBAction/Bai3/Bai3OAViewController.swift
+++ BaiTap04/ThanhNX/IBOutletAndIBAction/IBOutletAndIBAction/Bai3/Bai3OAViewController.swift
@@ -37,6 +37,47 @@ class Bai3OAViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var x: CGFloat = 50
+        var y: CGFloat = UIScreen.main.bounds.height / 5
+        for index in 0..<users.count {
+            let frame = CGRect(x: x, y: y, width: Config.nameWidth, height: Config.nameHeight)
+            let containerView = addAvatarView(frame: frame, name: users[index].name, avatarImage: users[index].avatar, tag: index)
+            view.addSubview(containerView)            
+            
+            x += 120
+            if x >= UIScreen.main.bounds.width - Config.nameWidth {
+                x = 50
+                y += 150
+            }
+        }
+    }
+    
+    func addAvatarView(frame: CGRect, name: String, avatarImage: UIImage, tag: Int) -> UIView {
+        let containerView = UIView(frame: frame)
+        containerView.backgroundColor = .white
         
+        let userAvatarImageView = UIImageView(image: avatarImage)
+        userAvatarImageView.frame = CGRect(x: 0, y: 0, width: 90, height: 100)
+        userAvatarImageView.contentMode = .scaleToFill
+        containerView.addSubview(userAvatarImageView)
+        
+        let avatarImageButton = UIButton(frame: CGRect(x: 0, y: 0, width: 90, height: 100))
+        avatarImageButton.backgroundColor = .clear
+        avatarImageButton.addTarget(self, action: #selector(buttonDidClick), for: .touchUpInside)
+        avatarImageButton.tag = tag
+        containerView.addSubview(avatarImageButton)
+        
+        let userNameLabel = UILabel(frame: CGRect(x: 0, y: 100, width: 90, height: 30))
+        userNameLabel.text = name
+        userNameLabel.textAlignment = .center
+        userNameLabel.backgroundColor = UIColor(red: 116 / 255, green: 185 / 255, blue: 255 / 255, alpha: 1)
+        userNameLabel.textColor = .white
+        containerView.addSubview(userNameLabel)
+        
+        return containerView
+    }
+    
+    @objc private func buttonDidClick(_ button: UIButton) {
+        print("\(users[button.tag].name)")
     }
 }
