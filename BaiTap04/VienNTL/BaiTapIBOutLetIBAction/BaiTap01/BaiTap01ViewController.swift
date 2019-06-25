d39e9f13487c5c225ac97c2488aaaf3dc328d3af
Vien Nguyen T. L <vien.nguyen2@asiantech.vn>
2019-06-25 11:48:23 +0700

    Nộp Bài tập 01

---
+++ BaiTap04/VienNTL/BaiTapIBOutLetIBAction/BaiTap01/BaiTap01ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BaiTap01ViewController.swift
+//  BaiTapIBOutLetIBAction
+//
+//  Created by Nguyen Truong Lam Vien on 6/25/19.
+//  Copyright © 2019 Nguyen Truong Lam Vien. All rights reserved.
+//
+
+import UIKit
+
+class BaiTap01ViewController: UIViewController {
+
+    @IBOutlet weak var dateTime: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func changeTime(_ button: UIButton) {
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss dd/MM/yyyy"
+        button.setTitle("\(formatter.string(from: currentDateTime))", for: .normal)
+    }
+    
+    //Touch Event
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch Began")
+        if let touch = touches.first {
+            let position = touch.location(in: self.view)
+            print("\(position.x),\(position.y)")
+        }
+        changeTime(dateTime)
+    }
+}
