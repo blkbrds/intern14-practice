e10fb71e231f0eecd87bbc2ac5f95b7d119db83e
VuNM <vu.nguyen2@asiantech.vn>
2019-06-26 10:22:52 +0700

    View exercise 1-5

---
+++ BaiTap03/VuNM/BaiTapView/BaiTapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTapView
+//
+//  Created by PCI0007 on 6/26/19.
+//  Copyright © 2019 PCI0007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
