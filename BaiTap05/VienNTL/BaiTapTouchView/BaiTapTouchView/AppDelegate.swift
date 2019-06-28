3142673fd47f55b2d0c49d9910f7450e433bd02f
Vien Nguyen T. L <vien.nguyen2@asiantech.vn>
2019-06-28 08:52:34 +0700

    no message

--- BaiTap05/VienNTL/BaiTapTouchView/BaiTapTouchView/AppDelegate.swift
+++ BaiTap05/VienNTL/BaiTapTouchView/BaiTapTouchView/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.backgroundColor = .white
         window?.makeKeyAndVisible()
         
-        let home = BaiTap01ViewController()
+        let home = BaiTap02ViewController()
         window?.rootViewController = home
         return true
     }
