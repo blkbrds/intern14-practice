216a0e5733cb7d592d9c567a1a2299ec33c0c21d
PCI0001 <tu.nguyen2@asiantech.vn>
2019-06-28 10:58:42 +0700

    Bai5 OutletAction TuNHT

--- BaiTapOutlet&Action/BaiTapOutlet&Action/BaiTap5.swift
+++ BaiTapOutlet&Action/BaiTapOutlet&Action/BaiTap5.swift
@@ -10,34 +10,27 @@ import UIKit
 
 class BaiTap5: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var backgroundLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
-    var red: CGFloat = 0
-    var green: CGFloat = 0
-    var blue: CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        titleLabel.text = "Color(R: , G: , B: )"
+        titleLabel.text = ""
         titleLabel.textColor = .black
-
+        
+        backgroundView.layer.cornerRadius = 20
+        backgroundView.layer.borderWidth = 1
+        backgroundView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        backgroundView.backgroundColor = UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
+        titleLabel.text = "Color(R: \(Int(red)), G: \(Int(green)), B: \(Int(blue)))"
     }
     
-    @IBAction func redButtonValueChanged(_ sender: UISlider) {
-        red = CGFloat(sender.value)
-        backgroundLabel.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
-    }
-    
-    @IBAction func greenButtonValueChanged(_ sender: UISlider) {
-        green = CGFloat(sender.value)
-        backgroundLabel.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
-    }
-    
-    @IBAction func blueButtonValueChanged(_ sender: UISlider) {
-        blue = CGFloat(sender.value)
-        backgroundLabel.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    @IBAction private func redButtonValueChanged(_ sender: UISlider) {
+        guard let red = redSlider?.value, let green = greenSlider?.value, let blue = blueSlider?.value else {
+            return
+        }
+        designView(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue))
     }
 }
