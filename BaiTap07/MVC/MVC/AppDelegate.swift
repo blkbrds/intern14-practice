4f5fd9ed7280ac52fbda2424463ef9bc57d45c2b
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-07-08 15:54:00 +0700

    Duong.Nguyen commit homework MVC

---
+++ BaiTap07/MVC/MVC/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MVC
+//
+//  Created by Nguyen Duong on 7/4/19.
+//  Copyright © 2019 Nguyen Duong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.backgroundColor = .white
+            window.makeKeyAndVisible()
+            let viewController = CalculatorViewController()
+            window.rootViewController = viewController
+        }
+        return true
+    }
+}
+
