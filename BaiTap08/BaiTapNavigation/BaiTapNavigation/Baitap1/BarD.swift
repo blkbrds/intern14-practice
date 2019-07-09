387b352afe3b5e3f7148d305651bf2da29d52c5e
PCI0001 <tu.nguyen2@asiantech.vn>
2019-07-09 17:33:05 +0700

    Navigation Baitap1 TuNHT

---
+++ BaiTap08/BaiTapNavigation/BaiTapNavigation/Baitap1/BarD.swift
@@ -0,0 +1,40 @@
+//
+//  ViewControllerD.swift
+//  BaiTapNavigation
+//
+//  Created by PCI0001 on 7/9/19.
+//  Copyright © 2019 PCI0001. All rights reserved.
+//
+
+import UIKit
+
+class BarD: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "ViewController D"
+        navigationController?.navigationBar.backgroundColor = .cyan
+    }
+    
+    @IBAction func nextButtonTouchUpInside(_ sender: UIButton) {
+        let viewController = BarE()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    @IBAction func cButtonTouchUpInside(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func bButtonTouchUpInside(_ sender: UIButton) {
+        for viewController in self.navigationController!.viewControllers as Array {
+            if viewController.isKind(of: BarB.self) {
+                self.navigationController!.popToViewController(viewController, animated: true)
+                break
+            }
+        }
+    }
+    
+    @IBAction func rootButtonTouchUpInside(_ sender: UIButton) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
---
+++ BaiTap08/BaiTapNavigation/BaiTapNavigation/Baitap1/BarE.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerE.swift
+//  BaiTapNavigation
+//
+//  Created by PCI0001 on 7/9/19.
+//  Copyright © 2019 PCI0001. All rights reserved.
+//
+
+import UIKit
+
+class BarE: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "ViewController E"
+        navigationController?.navigationBar.backgroundColor = .cyan
+    }
+    
+    @IBAction func cButtonTouchUpInside(_ sender: UIButton) {
+        for viewController in self.navigationController!.viewControllers as Array {
+            if viewController.isKind(of: BarC.self) {
+                self.navigationController!.popToViewController(viewController, animated: true)
+                break
+            }
+        }
+    }
+    
+    @IBAction func bButtonTouchUpInside(_ sender: UIButton) {
+        for viewController in self.navigationController!.viewControllers as Array {
+            if viewController.isKind(of: BarB.self) {
+                self.navigationController!.popToViewController(viewController, animated: true)
+                break
+            }
+        }
+    }
+    
+    @IBAction func dButtonTouchUpInside(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func rootButtonTouchUpInside(_ sender: UIButton) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
