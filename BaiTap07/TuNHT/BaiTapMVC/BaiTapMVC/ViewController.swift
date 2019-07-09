59df8bf7fe9a39cc289c7afcef4ed3422619c90f
PCI0001 <tu.nguyen2@asiantech.vn>
2019-07-09 15:19:24 +0700

    BaitapMVC with no model

---
+++ BaiTap07/TuNHT/BaiTapMVC/BaiTapMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTapMVC
+//
+//  Created by PCI0001 on 7/3/19.
+//  Copyright © 2019 PCI0001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
