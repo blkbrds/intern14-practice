7463b9b9daea23a7a347207ed7aa2becb9929d57
VuNM <vu.nguyen2@asiantech.vn>
2019-07-11 08:34:38 +0700

    no message

--- BaiTap04/VuNM/BaitapIBaction/BaitapIBaction/AppDelegate.swift
+++ BaiTap04/VuNM/BaitapIBaction/BaitapIBaction/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.backgroundColor = .white
         window?.makeKeyAndVisible()
         
-        let homeViewController = Ex02()
+        let homeViewController = Ex04()
         window?.rootViewController = homeViewController
         return true
     }
