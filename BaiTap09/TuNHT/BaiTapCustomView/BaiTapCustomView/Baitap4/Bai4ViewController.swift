ddb2a5f471a8df713232d23402dde7a8f4f19cb1
PCI0001 <tu.nguyen2@asiantech.vn>
2019-07-29 14:44:24 +0700

    Update Baitap4 TuNHT

--- BaiTap09/TuNHT/BaiTapCustomView/BaiTapCustomView/Baitap4/Bai4ViewController.swift
+++ BaiTap09/TuNHT/BaiTapCustomView/BaiTapCustomView/Baitap4/Bai4ViewController.swift
@@ -16,19 +16,23 @@ class Bai4ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        emailButton = ButtonView(frame: CGRect(x: 130, y: 200, width: 150, height: 100))
+        emailButton = ButtonView(frame: CGRect(x: 130, y: 200, width: 150, height: 100), value: 5, badgePosition: .topLeft)
+        emailButton?.titleLabel?.text = "Email"
         emailButton?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         emailButton?.layer.cornerRadius = 10
         view.addSubview(emailButton!)
         
-        friendButton = ButtonView(frame: CGRect(x: 130, y: 330, width: 150, height: 100))
+        friendButton = ButtonView(frame: CGRect(x: 130, y: 330, width: 150, height: 100), value: 46, badgePosition: .centerRight)
+        friendButton?.titleLabel?.text = "Friends"
         friendButton?.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         friendButton?.layer.cornerRadius = 10
         view.addSubview(friendButton!)
         
-        photoButton = ButtonView(frame: CGRect(x: 130, y: 460, width: 150, height: 100))
+        photoButton = ButtonView(frame: CGRect(x: 130, y: 460, width: 150, height: 100), value: 100, badgePosition: .bottomCenter)
+        photoButton?.titleLabel?.text = "Photos"
         photoButton?.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         photoButton?.layer.cornerRadius = 10
         view.addSubview(photoButton!)
+        
     }
 }
--- BaiTap09/TuNHT/BaiTapCustomView/BaiTapCustomView/Baitap4/ButtonView.swift
+++ BaiTap09/TuNHT/BaiTapCustomView/BaiTapCustomView/Baitap4/ButtonView.swift
@@ -8,27 +8,168 @@
 
 import UIKit
 
+enum BadgePosition {
+    case none
+    case topLeft
+    case topRight
+    case topCenter
+    case centerLeft
+    case centerRight
+    case bottomLeft
+    case bottomRight
+    case bottomCenter
+}
+
 class ButtonView: UIView {
     var button: UIButton?
+    var titleLabel: UILabel?
+    var badgeLabel: UILabel?
+    var value: Int
+    var badgePosition: BadgePosition
     
-    override init(frame: CGRect) {
+    init(frame: CGRect, value: Int, badgePosition: BadgePosition) {
+        self.value = value
+        self.badgePosition = badgePosition
         super.init(frame: frame)
         
         button = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 100))
         button?.layer.borderWidth = 1
         button?.layer.cornerRadius = 10
-        button?.titleLabel?.text = "Email"
-        button?.titleLabel?.textColor = .white
-        button?.titleLabel?.textAlignment = .center
-        button?.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         addSubview(button!)
+        
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 150, height: 100))
+        titleLabel?.layer.cornerRadius = 10
+        titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        titleLabel?.textColor = .white
+        titleLabel?.textAlignment = .center
+        addSubview(titleLabel!)
+        
+        badgeLabel = UILabel()
+        switch badgePosition {
+        case .none, .topLeft:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -15, y: -15, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -20, y: -15, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: -25, y: -15, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .topRight:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 135, y: -15, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 130, y: -15, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: 125, y: -15, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .topCenter:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 60, y: -15, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 55, y: -15, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: 50, y: -15, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .centerLeft:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -15, y: 35, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -20, y: 35, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: -25, y: 35, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .centerRight:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 135, y: 35, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 130, y: 35, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: 125, y: 35, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .bottomLeft:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -15, y: 85, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: -20, y: 85, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: -25, y: 85, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .bottomRight:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 135, y: 85, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 130, y: 85, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: 125, y: 85, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        case .bottomCenter:
+            if value < 10 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 60, y: 85, width: 30, height: 30)
+                badgeLabel?.layer.cornerRadius = (badgeLabel?.bounds.width)! / 2
+            } else if value <= 99 {
+                badgeLabel?.text = "\(value)"
+                badgeLabel?.frame = CGRect(x: 55, y: 85, width: 40, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            } else {
+                badgeLabel?.text = "99+"
+                badgeLabel?.frame = CGRect(x: 50, y: 85, width: 50, height: 30)
+                badgeLabel?.layer.cornerRadius = 10
+            }
+        }
+        badgeLabel?.layer.masksToBounds = true
+        badgeLabel?.textAlignment = .center
+        badgeLabel?.backgroundColor = #colorLiteral(red: 1, green: 0.01208707165, blue: 0, alpha: 1)
+        badgeLabel?.textColor = .white
+        
+        if value > 0 {
+            addSubview(badgeLabel!)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc func buttonTouchUpInside() {
-        
     }
 }
