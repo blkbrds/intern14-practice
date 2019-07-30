c727d327a868490f27456d49e8da7a0ec9cd0979
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-07-30 14:02:51 +0700

    Duong.Nguyen commit homework 03 of Customview

--- BaiTap09/DuongNT/CustomView/CustomView/Controllers/Ex3/Ex3ViewController.swift
+++ BaiTap09/DuongNT/CustomView/CustomView/Controllers/Ex3/Ex3ViewController.swift
@@ -9,87 +9,92 @@
 import UIKit
 
 class Ex3ViewController: BaseViewController {
-    
-    var exercise: Exercise?
-    fileprivate let nhieDoSlider = MySliderView()
-    fileprivate let doAmSlider = MySliderView()
-    fileprivate let sucGioSlider = MySliderView()
+
+    // MARK: - outlet
+
     @IBOutlet var sliderTextFields: [UITextField]!
     @IBOutlet weak var nhietDoView: UIView!
     @IBOutlet weak var doAmView: UIView!
     @IBOutlet weak var sucGioView: UIView!
-    
+
+    // MARK: - properties
+
+    var exercise: Exercise?
+    fileprivate let nhieDoSlider = MySliderView()
+    fileprivate let doAmSlider = MySliderView()
+    fileprivate let sucGioSlider = MySliderView()
+
+     // MARK: - life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     // MARK: config
+
     override func setupUI() {
         super.setupUI()
         self.title = exercise?.name
         setUpSlider()
     }
-    
+
     override func setupData() {
     }
-    
+
+    // MARK: - customer func
+
     private func setUpSlider() {
+
         let sliderView = UIView(frame: CGRect(x: 0, y: 0, width: 222, height: 90))
+
         nhieDoSlider.frame = sliderView.bounds
         nhieDoSlider.delegate = self
         nhieDoSlider.rightSliderView.backgroundColor = .red
         nhietDoView.addSubview(nhieDoSlider)
-        
+
         doAmSlider.frame = sliderView.bounds
         doAmSlider.delegate = self
         doAmSlider.rightSliderView.backgroundColor = .blue
-
         doAmView.addSubview(doAmSlider)
+
         sucGioSlider.frame = sliderView.bounds
         sucGioSlider.delegate = self
         sucGioSlider.rightSliderView.backgroundColor = .green
         sucGioView.addSubview(sucGioSlider)
-            
-        sliderTextFields.forEach {
-            textField in
+
+        sliderTextFields.forEach { textField in
             switch textField.tag {
             case 1:
                 textField.text = ("\(Int(nhieDoSlider.valueOfThumbnai))")
-                break
             case 2:
                 textField.text = ("\(Int(doAmSlider.valueOfThumbnai))")
-                break
             case 3:
                 textField.text = ("\(Int(sucGioSlider.valueOfThumbnai))")
-                break
             default:
                 textField.text = ""
             }
         }
     }
-    
+
+    // MARK: - IBAction function
+
     @IBAction func valueEditChanged(_ sender: Any) {
-        sliderTextFields.forEach {
-            textField in
+        sliderTextFields.forEach { textField in
             guard let value = textField.text else {
                 return
             }
             switch textField.tag {
             case 1:
                 nhieDoSlider.valueChanged(value: Float(value) ?? 0.0)
-                break
             case 2:
                 doAmSlider.valueChanged(value: Float(value) ?? 0.0)
-                break
             case 3:
                 sucGioSlider.valueChanged(value: Float(value) ?? 0.0)
-                break
-            default:
-                break
+            default: break
             }
         }
     }
@@ -99,18 +104,14 @@ class Ex3ViewController: BaseViewController {
 
 extension Ex3ViewController: MySliderViewDelegate {
     func mySlider(_ mySlider: MySliderView, percent: Int) {
-        sliderTextFields.forEach {
-            textField in
+        sliderTextFields.forEach { textField in
             switch textField.tag {
             case 1:
                 textField.text = ("\(Int(nhieDoSlider.valueOfThumbnai))")
-                break
             case 2:
                 textField.text = ("\(Int(doAmSlider.valueOfThumbnai))")
-                break
             case 3:
                 textField.text = ("\(Int(sucGioSlider.valueOfThumbnai))")
-                break
             default:
                 textField.text = ""
             }
