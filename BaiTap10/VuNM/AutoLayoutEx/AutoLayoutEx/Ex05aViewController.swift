e4bc4c84a260240225cd402a852517cdc802cf2c
VuNM <vu.nguyen2@asiantech.vn>
2019-08-05 10:14:28 +0700

    Have done to Ex05d

---
+++ BaiTap10/VuNM/AutoLayoutEx/AutoLayoutEx/Ex05aViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Ex05aViewController.swift
+//  AutoLayoutEx
+//
+//  Created by PCI0007 on 8/5/19.
+//  Copyright © 2019 PCI0007. All rights reserved.
+//
+
+import UIKit
+
+class Ex05aViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
