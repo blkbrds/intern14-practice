64fb7d60a87be1332bd5caf496a9e9f5651722a0
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-08-05 10:37:15 +0700

    Duong.Nguyen commit homework 04 of auto layout

--- BaiTap10/DuongNT/AutoLayout/AutoLayout/Controllers/Ex4/Ex4ViewController.swift
+++ BaiTap10/DuongNT/AutoLayout/AutoLayout/Controllers/Ex4/Ex4ViewController.swift
@@ -9,23 +9,54 @@
 import UIKit
 
 class Ex4ViewController: BaseViewController {
+
+    // MARK: - Outlets
+    @IBOutlet var buttons: [UIButton]!
     
+    // MARK: - Properties
     var exercise: Exercise?
-    
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    // MARK: config
+
+    // MARK: - Config
     override func setupUI() {
         super.setupUI()
         self.title = exercise?.name
+        buttons.forEach { button in
+            button.layer.cornerRadius = 5
+        }
     }
-    
+
     override func setupData() {
+    }
+
+    // MARK: - Actions
+    @IBAction func buttonsActionTouchUpInside(_ button: UIButton) {
+        switch button.tag {
+        case 1:
+            let baiTap4 = BaiTap4ViewController()
+            navigationController?.pushViewController(baiTap4, animated: true)
+        case 2:
+            let baiTap42 = BaiTap42ViewController()
+            navigationController?.pushViewController(baiTap42, animated: true)
+        case 3:
+            let baiTap43 = BaiTap43ViewController()
+            navigationController?.pushViewController(baiTap43, animated: true)
+        case 4:
+            let baiTap44 = BaiTap44ViewController()
+            navigationController?.pushViewController(baiTap44, animated: true)
+        case 5:
+            let baiTap45 = BaiTap45ViewController()
+            navigationController?.pushViewController(baiTap45, animated: true)
+        default:
+            break
+        }
     }
 }
