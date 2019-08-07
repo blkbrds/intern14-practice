db01f138d14d23916e41349c2855b6ed652d281d
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-08-07 17:34:22 +0700

    Update bai 9 Auto Layout

---
+++ BaiTap10/ThanhNX/AutoLayoutBasic/AutoLayoutBasic/Bai10VC.swift
@@ -0,0 +1,16 @@
+//
+//  Bai10VC.swift
+//  AutoLayoutBasic
+//
+//  Created by PCI0008 on 8/7/19.
+//  Copyright © 2019 PCI0008. All rights reserved.
+//
+
+import UIKit
+
+final class Bai10VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
