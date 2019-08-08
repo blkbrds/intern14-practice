6e487a7c428c2c6bdf537e654687d9deaadbe249
VuNM <vu.nguyen2@asiantech.vn>
2019-08-08 09:53:20 +0700

    done test 2

---
+++ KiemTraLan2/Vu/test2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class DongVat {
+    var ten: String
+    var cannang: Double
+    var chan: Int
+    
+    init(ten: String, cannang: Double, chan: Int) {
+        self.cannang = cannang
+        self.ten = ten
+        self.chan = chan
+    }
+}
+
+class Ga: DongVat {
+    
+}
---
+++ KiemTraLan2/Vu/test2/test2/Info/InfoViewController.swift
@@ -0,0 +1,58 @@
+//
+//  InfoViewController.swift
+//  test2
+//
+//  Created by PCI0007 on 8/5/19.
+//  Copyright © 2019 PCI0007. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    
+    var customPopUp = PopUp()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let popUp = Bundle.main.loadNibNamed("PopUp", owner: self, options: nil)?[0] as? PopUp {
+            self.customPopUp = popUp
+            popUp.frame = CGRect(x: 0, y: 100, width: view.bounds.width, height: popUp.bounds.height)
+            self.view.addSubview(popUp)
+        }
+        nameTextField.delegate = self
+        nameTextField.isHidden = false
+        
+        customPopUp.delegate = self
+        customPopUp.isHidden = false
+    }
+
+
+    @IBAction func editButtonTouchUpInside(_ sender: UIButton) {
+        customPopUp.isHidden = false
+        customPopUp.show(animation: true)
+    }
+    
+
+}
+
+extension InfoViewController: UITextFieldDelegate {
+
+}
+
+extension InfoViewController: PopUpDelegate {
+    func view(view: PopUp, needPerformAction action: PopUp.Action, name: String?) {
+        switch action {
+        case .done:
+            print("tap")
+        case .showName:
+            nameTextField.text = name
+        }
+    }
+}
+
