4bcda8c04f3f36efe58411fa953019dab517b504
VuNM <vu.nguyen2@asiantech.vn>
2019-08-08 16:28:58 +0700

    Rename ex 10's lay out

--- BaiTap10/VuNM/AutoLayoutEx/AutoLayoutEx/Ex10/Ex10ViewController.swift
+++ BaiTap10/VuNM/AutoLayoutEx/AutoLayoutEx/Ex10/Ex10ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class Ex10ViewController: UIViewController {
     
-    @IBOutlet weak var heigtButton4n3: NSLayoutConstraint!
-    @IBOutlet weak var widthButton4n3: NSLayoutConstraint!
-    @IBOutlet weak var heightButton3n2: NSLayoutConstraint!
-    @IBOutlet weak var heightButton2n1: NSLayoutConstraint!
-    @IBOutlet weak var widthButton3n2: NSLayoutConstraint!
-    @IBOutlet weak var widthButton2n1: NSLayoutConstraint!
-    @IBOutlet weak var withdButton4n1: NSLayoutConstraint!
-    @IBOutlet weak var heightButton4n1: NSLayoutConstraint!
+    @IBOutlet weak var heigtButton4n3Layout: NSLayoutConstraint!
+    @IBOutlet weak var widthButton4n3Layout: NSLayoutConstraint!
+    @IBOutlet weak var heightButton3n2Layout: NSLayoutConstraint!
+    @IBOutlet weak var heightButton2n1Layout: NSLayoutConstraint!
+    @IBOutlet weak var widthButton3n2Layout: NSLayoutConstraint!
+    @IBOutlet weak var widthButton2n1Layout: NSLayoutConstraint!
+    @IBOutlet weak var withdButton4n1Layout: NSLayoutConstraint!
+    @IBOutlet weak var heightButton4n1Layout: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,37 +25,37 @@ class Ex10ViewController: UIViewController {
     }
     
     func standardValue() {
-        widthButton4n3.priority = UILayoutPriority(750)
-        widthButton3n2.priority = UILayoutPriority(750)
-        widthButton2n1.priority = UILayoutPriority(750)
-        withdButton4n1.priority = UILayoutPriority(750)
-        heigtButton4n3.priority = UILayoutPriority(250)
-        heightButton3n2.priority = UILayoutPriority(250)
-        heightButton2n1.priority = UILayoutPriority(250)
-        heightButton4n1.priority = UILayoutPriority(250)
+        widthButton4n3Layout.priority = UILayoutPriority(750)
+        widthButton3n2Layout.priority = UILayoutPriority(750)
+        widthButton2n1Layout.priority = UILayoutPriority(750)
+        withdButton4n1Layout.priority = UILayoutPriority(750)
+        heigtButton4n3Layout.priority = UILayoutPriority(250)
+        heightButton3n2Layout.priority = UILayoutPriority(250)
+        heightButton2n1Layout.priority = UILayoutPriority(250)
+        heightButton4n1Layout.priority = UILayoutPriority(250)
     }
 
     @IBAction func userTapTouchUpInside(_ sender: UIButton) {
         if sender.tag == 0 {
             standardValue()
-            heigtButton4n3.priority = UILayoutPriority(999)
-            widthButton3n2.priority = UILayoutPriority(999)
-            widthButton2n1.priority = UILayoutPriority(999)
+            heigtButton4n3Layout.priority = UILayoutPriority(999)
+            widthButton3n2Layout.priority = UILayoutPriority(999)
+            widthButton2n1Layout.priority = UILayoutPriority(999)
         } else if sender.tag == 1 {
             standardValue()
-            heightButton3n2.priority = UILayoutPriority(999)
-            widthButton2n1.priority = UILayoutPriority(999)
-            withdButton4n1.priority = UILayoutPriority(999)
+            heightButton3n2Layout.priority = UILayoutPriority(999)
+            widthButton2n1Layout.priority = UILayoutPriority(999)
+            withdButton4n1Layout.priority = UILayoutPriority(999)
         } else if sender.tag == 2 {
             standardValue()
-            heightButton2n1.priority = UILayoutPriority(999)
-            withdButton4n1.priority = UILayoutPriority(999)
-            widthButton4n3.priority = UILayoutPriority(999)
+            heightButton2n1Layout.priority = UILayoutPriority(999)
+            withdButton4n1Layout.priority = UILayoutPriority(999)
+            widthButton4n3Layout.priority = UILayoutPriority(999)
         } else {
             standardValue()
-            heightButton4n1.priority = UILayoutPriority(999)
-            widthButton3n2.priority = UILayoutPriority(999)
-            widthButton4n3.priority = UILayoutPriority(999)
+            heightButton4n1Layout.priority = UILayoutPriority(999)
+            widthButton3n2Layout.priority = UILayoutPriority(999)
+            widthButton4n3Layout.priority = UILayoutPriority(999)
         }
         
         UIView.animate(withDuration: 0.5) {
