7850f3f2bedb4379a094aeaee69067e557f13e18
VuNM <vu.nguyen2@asiantech.vn>
2019-08-09 11:55:48 +0700

    complete Ex 12 and 13

---
+++ BaiTap10/VuNM/AutoLayoutEx/AutoLayoutEx/Ex14/Ex14ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Ex14ViewController.swift
+//  AutoLayoutEx
+//
+//  Created by PCI0007 on 8/9/19.
+//  Copyright © 2019 PCI0007. All rights reserved.
+//
+
+import UIKit
+
+class Ex14ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
