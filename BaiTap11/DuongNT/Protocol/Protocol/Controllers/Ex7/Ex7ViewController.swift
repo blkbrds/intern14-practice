e6d68585ef1ecbb9eb48ce781ac86d3b7c73c0af
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-08-09 14:16:21 +0700

    Duong.Nguyen commit template of protocols

---
+++ BaiTap11/DuongNT/Protocol/Protocol/Controllers/Ex7/Ex7ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Ex7ViewController.swift
+//  PracticeTemplate
+//
+//  Created by Tien Le P. on 6/22/18.
+//  Copyright © 2018 Tien Le P. All rights reserved.
+//
+
+import UIKit
+
+class Ex7ViewController: BaseViewController {
+
+    // MARK: - Properties
+    var exercise: Exercise?
+
+    // MARK: - Life Cicle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Config
+    override func setupUI() {
+        super.setupUI()
+        self.title = exercise?.name
+    }
+
+    override func setupData() {
+        super.setupData()
+    }
+}
