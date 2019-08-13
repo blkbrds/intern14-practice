f5e242bafa360c4091b0d5faaa2a108f0e5d6b9e
Thanh Nguyen Xuan <thanh.nguyen4@asiantech.vn>
2019-08-13 16:30:17 +0700

    Update bai 1 Protocol

--- BaiTap11/ThanhNX/PracticeTemplate/Bai1Protocol.playground/Contents.swift
+++ BaiTap11/ThanhNX/PracticeTemplate/Bai1Protocol.playground/Contents.swift
@@ -1,39 +1,132 @@
 import UIKit
 
-final class Thu {
-    var isDi: Bool
-    var ten: String
+protocol Thu {
+    var isDi: Bool { set get }
+}
+
+protocol Chim {
+    var isBay: Bool { set get }
+}
+
+protocol Ca {
+    var isBoi: Bool { set get }
+}
+
+protocol DongVat: Thu, Chim, Ca {
+    var ten: String { set get }
+}
+
+final class CaChuon: DongVat {
+    var ten: String = "Ca Chuon"
     
-    init(isDi: Bool, ten: String) {
-        self.isDi = isDi
-        self.ten = ten
+    var isDi: Bool = false
+    
+    var isBay: Bool = true
+    
+    var isBoi: Bool = true
+}
+
+final class Bo: DongVat {
+    var isDi: Bool = true
+    
+    var isBay: Bool = false
+    
+    var isBoi: Bool = false
+    
+    var ten: String = "Bo"
+}
+
+final class Ga: DongVat {
+    var ten: String = "Ga"
+    
+    var isDi: Bool = true
+    
+    var isBay: Bool = true
+    
+    var isBoi: Bool = false
+}
+
+final class Vit: DongVat {
+    var ten: String = "Vit"
+    
+    var isDi: Bool = true
+    
+    var isBay: Bool = true
+    
+    var isBoi: Bool = true
+}
+
+final class CaMap: DongVat {
+    var ten: String = "Ca Map"
+    
+    var isDi: Bool = false
+    
+    var isBay: Bool = false
+    
+    var isBoi: Bool = true
+}
+
+final class Heo: DongVat {
+    var ten: String = "Heo"
+    
+    var isDi: Bool = true
+    
+    var isBay: Bool = false
+    
+    var isBoi: Bool = false
+}
+
+final class HaiCau: DongVat {
+    var ten: String = "Hai Cau"
+    
+    var isDi: Bool = true
+    
+    var isBay: Bool = false
+    
+    var isBoi: Bool = true
+}
+
+final class Doi: DongVat {
+    var ten: String = "Doi"
+    
+    var isDi: Bool = false
+    
+    var isBay: Bool = true
+    
+    var isBoi: Bool = false
+}
+
+final class CaSau: DongVat {
+    var ten: String = "Ca Sau"
+    
+    var isDi: Bool = true
+    
+    var isBay: Bool = false
+    
+    var isBoi: Bool = true
+}
+
+let arrAnimal: [DongVat] = [Bo(), Vit(), CaChuon(), CaMap(), CaSau(), Heo(), Doi(), Ga(), HaiCau()]
+var arrBoi: [DongVat] = []
+var arrBay: [DongVat] = []
+var arrDi: [DongVat] = []
+
+func getResult() {
+    for animal in arrAnimal {
+        if animal.isDi {
+            arrDi.append(animal)
+        }
+        if animal.isBay {
+            arrBay.append(animal)
+        }
+        if animal.isBoi {
+            arrBoi.append(animal)
+        }
     }
 }
 
-final class Chim {
-    var isBay: Bool
-    var ten: String
-    
-    init(isBay: Bool, ten: String) {
-        self.isBay = isBay
-        self.ten = ten
-    }
-}
+getResult()
 
-final class Ca {
-    var isBoi: Bool
-    var ten: String
-    
-    init(isBoi: Bool, ten: String) {
-        self.isBoi = isBoi
-        self.ten = ten
-    }
-}
-
-//MARK: Lop Thu
-var bo = Thu(isDi: true, ten: "Bo")
-var heo = Thu(isDi: true, ten: "Heo")
-var haiCau = Thu(isDi: false, ten: "Hai Cau")
-var doi = Thu(isDi: false, ten: "Doi")
-var caSau = Thu(isDi: true, ten: "Ca Sau")
-
+print(arrBoi)
+print(arrBay)
+print(arrDi)
