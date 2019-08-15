05ac607804f767d1d89bab9094d79c74024e8906
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-08-15 12:08:58 +0700

    commit homework 03 table view

--- BaiTap12/DuongNT/TableView/TableView/Controllers/Ex3/Ex3ViewController.swift
+++ BaiTap12/DuongNT/TableView/TableView/Controllers/Ex3/Ex3ViewController.swift
@@ -9,26 +9,56 @@
 import UIKit
 
 class Ex3ViewController: BaseViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: - Properties
     var exercise: Exercise?
-
+    var users: [User] = []
+    
     // MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // MARK: - Config
     override func setupUI() {
         super.setupUI()
         self.title = exercise?.name
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
     override func setupData() {
         super.setupData()
+        users = DataManagement.share.getUser(fileName: "nameList", type: "plist")
+    }
+}
+extension Ex3ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier")
+        let user = users[indexPath.row]
+        cell?.textLabel?.text = user.name
+        return cell!
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = Ex3DetailViewController()
+        let user = users[indexPath.row]
+        vc.name = user.name
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
