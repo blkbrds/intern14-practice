b6ba4ed18c6ce303e802c2d6817d4f97e2631184
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-08-15 21:59:54 +0700

    commit homwork 07 table view

--- BaiTap12/DuongNT/TableView/TableView/Controllers/Ex6/Ex6ViewController.swift
+++ BaiTap12/DuongNT/TableView/TableView/Controllers/Ex6/Ex6ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class Ex6ViewController: BaseViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+
     // MARK: - Properties
     var exercise: Exercise?
+    private var users: [User] = []
+    private var arrayUsers: [[User]] = []
+    private var indexTableView: [String] = []
 
     // MARK: - Life Cicle
     override func viewDidLoad() {
@@ -26,9 +32,47 @@ class Ex6ViewController: BaseViewController {
     override func setupUI() {
         super.setupUI()
         self.title = exercise?.name
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func setupData() {
         super.setupData()
+        users = DataManagement.share.getUser(fileName: "nameList", type: "plist")
+        for i in 1...3 {
+            arrayUsers.append(users)
+            indexTableView.append("\(i)")
+        }
+    }
+}
+
+extension Ex6ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return arrayUsers.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayUsers[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let user = arrayUsers[indexPath.section][indexPath.row]
+        cell.textLabel?.text = user.name
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(section + 1) Section"
+    }
+
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return indexTableView
+    }
+
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
     }
 }
