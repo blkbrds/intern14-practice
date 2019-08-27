99a45d30001f44da3fa26272c663cca125b345d4
Nguyen Tung Duong <nguyentungduong2601@gmail.com>
2019-08-27 09:05:02 +0700

    tách API Model

--- BaiTap19/DuongNT/BaiTap2/BaiTap2/Models/AudioBooks/AudioBook.swift
+++ BaiTap19/DuongNT/BaiTap2/BaiTap2/Models/AudioBooks/AudioBook.swift
@@ -7,7 +7,9 @@
 //
 
 import Foundation
+import UIKit
 
 struct AudioBook {
-    let feed: [String: Any]?
+    var imageStr: String
+    var thumbnail: UIImage?
 }
--- BaiTap19/DuongNT/BaiTap2/BaiTap2/Views/TopAudioBooksViewController.swift
+++ BaiTap19/DuongNT/BaiTap2/BaiTap2/Views/TopAudioBooksViewController.swift
@@ -11,17 +11,12 @@ import UIKit
 class TopAudioBooksViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-
-    var listImage: [Img] = []
+    var books = [AudioBook]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        topAudioBooks {
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
+        loadData()
     }
 
     // MARK: - Private functions
@@ -38,65 +33,24 @@ class TopAudioBooksViewController: UIViewController {
         tableView.delegate = self
     }
 
-    private func convertToJSON(from data: Data) -> [String: Any] {
-        var json: [String: Any] = [:]
-        do {
-            if let jsonObj = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                json = jsonObj
-            }
-        } catch {
-            print("JSON casting error")
-        }
-        return json
-    }
-
-    func topAudioBooks(completion: @escaping () -> Void) {
-        guard let url = URL(string: Paths.topAudioBooks) else { return }
-        let config = URLSessionConfiguration.ephemeral
-        config.waitsForConnectivity = true
-        let session = URLSession(configuration: config)
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let data = data {
-                let json = self.convertToJSON(from: data)
-                guard let feed = json["feed"] as? [String: Any], let entries = feed["entry"] as? [[String: Any]], let listImg = entries[0]["im:image"] as? [[String: Any]] else {
-                    return
+    private func loadData() {
+        //api.delegate = self
+        
+        books.removeAll()
+        ApiManager.Audio.getTop { (result) in
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let audioBooks):
+                for book in audioBooks {
+                    self.books.append(book)
                 }
-                var imgs: [Img] = []
-                for index in listImg {
-                    let img = Img(label: index["label"] as? String, attribute: index["attributes"] as? [String: Any])
-                    imgs.append(img)
-                }
-                self.listImage = imgs
+                self.tableView.reloadData()
             }
-
-            if let response = response {
-                print(response)
-            }
-
-            if let error = error {
-                print(error)
-            }
-            completion()
         }
-        task.resume()
     }
-
-    func downloadImage(url: String, completion: @escaping (UIImage?) -> Void) {
-        guard let url = URL(string: url) else { return }
-        let config = URLSessionConfiguration.default
-        config.waitsForConnectivity = true
-        let session = URLSession(configuration: config)
-        let task = session.dataTask(with: url) { (data, response, error) in
-            var image: UIImage?
-            if let data = data {
-                image = UIImage(data: data)
-            }
-            completion(image)
-        }
-        task.resume()
-    }
+    
 }
-
 // MARK: - UITableViewDataSource
 extension TopAudioBooksViewController: UITableViewDataSource {
 
@@ -105,20 +59,32 @@ extension TopAudioBooksViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listImage.count
+        return books.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TopAudioBookTableViewCell else {
             return UITableViewCell()
         }
-        downloadImage(url: listImage[indexPath.row].label!) { (image) in
-            DispatchQueue.main.async {
-                cell.imgView.image = image
+
+        let book = books[indexPath.row]
+        cell.urlLabel.text = book.imageStr
+        
+        if let thumbnail = book.thumbnail {
+            cell.imgView.image = thumbnail
+        } else {
+            cell.imgView.image = nil
+            ApiManager.Downloader.downloadImage(imageURL: book.imageStr, index: indexPath.row) { (image, index) in
+                var book = self.books[index]
+                book.thumbnail = image
+                
+                let cell = tableView.cellForRow(at: indexPath) as? TopAudioBookTableViewCell
+                cell?.imgView.image = image
             }
         }
-        cell.urlLabel.text = listImage[indexPath.row].label
-        cell.attributeLabel.text = listImage[indexPath.row].attribute!["height"] as? String
+        
+        
+       
         return cell
     }
 }
@@ -127,7 +93,6 @@ extension TopAudioBooksViewController: UITableViewDataSource {
 extension TopAudioBooksViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Selected: \(listImage[indexPath.row])")
     }
 }
 
