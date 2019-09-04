23c9857943bba6b7032cd0a60da40311c069c3d2
PCI0010 <hung.pham@asiantech.vn>
2019-09-04 09:35:48 +0700

    Update Bai Tap 8-14 Auto Layout

---
+++ BaiTap10/HungPV/ExAutoLayout/ExAutoLayout/BaiTap13/Ex13ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Ex13ViewController.swift
+//  ExAutoLayout
+//
+//  Created by PCI0010 on 9/3/19.
+//  Copyright © 2019 PCI0010. All rights reserved.
+//
+
+import UIKit
+
+class Ex13ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = " Ex13 "
+    }
+}
---
+++ BaiTap10/HungPV/ExAutoLayout/ExAutoLayout/BaiTap14/Ex14ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Ex14ViewController.swift
+//  ExAutoLayout
+//
+//  Created by PCI0010 on 9/3/19.
+//  Copyright © 2019 PCI0010. All rights reserved.
+//
+
+import UIKit
+
+class Ex14ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = " Ex14 "
+    }
+
+
+  
+
+}
