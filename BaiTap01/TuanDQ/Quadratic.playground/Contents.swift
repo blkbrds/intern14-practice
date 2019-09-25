dbdcf0f381f3afe3ca99681975c1887ecd0a944c
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-09-25 11:04:41 +0700

    Change to switch case.

--- BaiTap01/TuanDQ/Quadratic.playground/Contents.swift
+++ BaiTap01/TuanDQ/Quadratic.playground/Contents.swift
@@ -2,11 +2,12 @@
 
 import UIKit
 
-func quadraticEquation(a firstNo:Int, b secondNo: Int, c thirthNo: Int) -> (msg: String?, result: [Double]) {
+func quadraticEquation(a firstNo:Float, b secondNo: Float, c thirthNo: Float) -> (msg: String?, result: [Float]) {
     
-    if (firstNo == 0) {
-        return ("the equation is not quadratic.", [])
-    } else {
+    switch (firstNo == 0, secondNo == 0, thirthNo == 0) {
+    case (true, false, _):
+        return simpleEquation(b: secondNo, c: thirthNo)
+    default:
         if (secondNo == 0) {
             if (thirthNo == 0) {
                 return (nil, [0])
@@ -14,17 +15,30 @@ func quadraticEquation(a firstNo:Int, b secondNo: Int, c thirthNo: Int) -> (msg:
                 return ("Have no result.", [])
             }
         } else {
-            let discriminant = Double(secondNo * secondNo - 4 * firstNo * thirthNo)
+            let discriminant = secondNo * secondNo - 4 * firstNo * thirthNo
             if (discriminant < 0) {
                 return ("Have no result.", [])
             } else if (discriminant == 0) {
-                return (nil, [Double(-1 * secondNo) / Double(2 * firstNo)])
+                return (nil, [(-1 * secondNo) / (2 * firstNo)])
             }
-            return (nil, [(Double(-1 * secondNo) + sqrt(discriminant)) / Double(2 * firstNo),
-                (Double(-1 * secondNo) - sqrt(discriminant)) / Double(2 * firstNo)])
+            return (nil, [((-1 * secondNo) + sqrt(discriminant)) / (2 * firstNo),
+                ((-1 * secondNo) - sqrt(discriminant)) / (2 * firstNo)])
         }
     }
 }
+
+func simpleEquation(b firstNo:Float, c secondNo: Float) -> (msg: String?, result: [Float]) {
+    if (firstNo == 0) {
+        if (secondNo == 0) {
+            return ("Have a lot of result.", [])
+        } else {
+            return ("Have no result.", [])
+        }
+    } else {
+        return (nil, [-1 * secondNo / firstNo])
+    }
+}
+
 
 let quadraticResult = quadraticEquation(a: 10, b: 17, c: 6)
 if (quadraticResult.msg == nil) {
