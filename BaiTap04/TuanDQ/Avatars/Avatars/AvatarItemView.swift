ccbb9e8373553326725deb430c9213dbc378f42a
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-09-30 14:38:20 +0700

    Update source code

---
+++ BaiTap04/TuanDQ/Avatars/Avatars/AvatarItemView.swift
@@ -0,0 +1,40 @@
+//
+//  AvatarItemView.swift
+//  Avatars
+//
+//  Created by MBA0051 on 9/30/19.
+//  Copyright © 2019 MBA0051. All rights reserved.
+//
+
+import UIKit
+
+class AvatarItemView: UIView {
+
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var avatarButton: UIButton!
+    
+    fileprivate var avatarImage : UIImage?
+    
+    @IBAction func avatarButtonClick(_ sender: Any) {
+        print(personNameLabel.text!)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /**
+     * Customize initial default.
+     */
+    required init(_ person: PersonInfo) {
+        super.init(frame: CGRect.zero)
+        self.personNameLabel?.text = person.personName
+        self.personImageView?.image = person.personImage
+        self.avatarButton?.tag = person.tag
+    }
+}
--- BaiTap04/TuanDQ/Avatars/Avatars/AvatarsViewController.swift
+++ BaiTap04/TuanDQ/Avatars/Avatars/AvatarsViewController.swift
@@ -9,65 +9,80 @@
 import UIKit
 
 class AvatarsViewController: UIViewController {
-
-    @IBOutlet weak var avatar1: UIImageView!
-    @IBOutlet weak var avatar2: UIImageView!
-    @IBOutlet weak var avatar3: UIImageView!
-    @IBOutlet weak var avatar4: UIImageView!
-    @IBOutlet weak var avatar5: UIImageView!
-    @IBOutlet weak var avatar6: UIImageView!
-    @IBOutlet weak var avatar7: UIImageView!
-    @IBOutlet weak var avatar8: UIImageView!
-    @IBOutlet weak var avatar9: UIImageView!
     
-    @IBOutlet weak var personName1: UILabel!
-    @IBOutlet weak var personName2: UILabel!
-    @IBOutlet weak var personName3: UILabel!
-    @IBOutlet weak var personName4: UILabel!
-    @IBOutlet weak var personName5: UILabel!
-    @IBOutlet weak var personName6: UILabel!
-    @IBOutlet weak var personName7: UILabel!
-    @IBOutlet weak var personName8: UILabel!
-    @IBOutlet weak var personName9: UILabel!
-    
-    @IBOutlet weak var personInfo1: UIButton!
-    @IBOutlet weak var personInfo2: UIButton!
-    @IBOutlet weak var personInfo3: UIButton!
-    @IBOutlet weak var personInfo4: UIButton!
-    @IBOutlet weak var personInfo5: UIButton!
-    @IBOutlet weak var personInfo6: UIButton!
-    @IBOutlet weak var personInfo7: UIButton!
-    @IBOutlet weak var personInfo8: UIButton!
-    @IBOutlet weak var personInfo9: UIButton!
+    var personsInfo: [AvatarItemView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        personsInfo = [
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 1", tag: 1)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 2", tag: 2)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 3", tag: 3)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 4", tag: 4)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 5", tag: 5)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 6", tag: 6)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 7", tag: 7)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 8", tag: 8)),
+            AvatarItemView(PersonInfo(personImage: UIImage(named: "avatar.png")!, personName: "User 9", tag: 9))
+        ]
+        
+        settingLayout()
+    }
+
+    private func settingLayout() {
+        
+        let leftMargin = 25
+        let topMargin = 125
+        var x:Int = leftMargin
+        var y:Int = 50
+        
+        let maxHeight = Int(UIScreen.main.bounds.size.height)
+        let maxWidth = Int(UIScreen.main.bounds.size.width)
+        // Calculate to display.
+        for person in personsInfo {
+            addComponent(x: x, y: y, person: person)
+            
+            // Move to new point.
+            x += Int(person.frame.width) + leftMargin
+            if (x + Int(person.frame.width) > maxWidth) {
+                x = leftMargin
+                y += topMargin
+            }
+            if (y > maxHeight - topMargin + leftMargin) {
+                break
+            }
+        }
+    }
+    
+    func addComponent(x xPoint: Int, y yPoint: Int, person personInfo: AvatarItemView) {
+        personInfo.frame = CGRect(origin: CGPoint(x: xPoint, y: yPoint), size: CGSize(width: personInfo.bounds.width, height: personInfo.bounds.height))
+        view.addSubview(personInfo)
     }
 
     @IBAction func viewPersonInformation(_ sender: UIButton) {
-        switch sender {
-        case personInfo1:
-            print("Avatar name : \(personName1.text!)")
-        case personInfo2:
-            print("Avatar name : \(personName2.text!)")
-        case personInfo3:
-            print("Avatar name : \(personName3.text!)")
-        case personInfo4:
-            print("Avatar name : \(personName4.text!)")
-        case personInfo5:
-            print("Avatar name : \(personName5.text!)")
-        case personInfo6:
-            print("Avatar name : \(personName6.text!)")
-        case personInfo7:
-            print("Avatar name : \(personName7.text!)")
-        case personInfo8:
-            print("Avatar name : \(personName8.text!)")
-        case personInfo9:
-            print("Avatar name : \(personName9.text!)")
-        default:
-            print("Have no information")
-        }
+//        switch sender {
+//        case personInfo1:
+//            print("Avatar name : \(personName1.text!)")
+//        case personInfo2:
+//            print("Avatar name : \(personName2.text!)")
+//        case personInfo3:
+//            print("Avatar name : \(personName3.text!)")
+//        case personInfo4:
+//            print("Avatar name : \(personName4.text!)")
+//        case personInfo5:
+//            print("Avatar name : \(personName5.text!)")
+//        case personInfo6:
+//            print("Avatar name : \(personName6.text!)")
+//        case personInfo7:
+//            print("Avatar name : \(personName7.text!)")
+//        case personInfo8:
+//            print("Avatar name : \(personName8.text!)")
+//        case personInfo9:
+//            print("Avatar name : \(personName9.text!)")
+//        default:
+//            print("Have no information")
+//        }
     }
 }
