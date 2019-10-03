e4640198a3be9033a192d21300ede209393e53e9
at-hoangpham <hoang.pham@asiantech.vn>
2019-10-03 17:07:02 +0700

    Bai 1

--- BaiTap02/HoangPT/B1.playground/Contents.swift
+++ BaiTap02/HoangPT/B1.playground/Contents.swift
@@ -1,15 +1,73 @@
 import UIKit
 
-class PhanSo {
-    var tuSo: Int
-    var mauSo: Int?
+class Fraction {
+    var numerator: Int
+    var denominator: Int
     
-    init(tuSo: Int, mauSo: Int) {
-        self.tuSo = tuSo
-        if mauSo == 0 {
-            self.mauSo = nil
+    init?(_ numerator: Int, _ denominator: Int) {
+        self.numerator = numerator
+        if denominator == 0 {
+            return nil
+        }
+        self.denominator = denominator
+    }
+    
+    func summation(fraction: Fraction?) -> Fraction? {
+        guard let fraction = fraction else { return nil }
+        if self.denominator == fraction.denominator {
+            return Fraction(self.numerator + fraction.numerator, self.denominator)
         } else {
-            self.mauSo = mauSo
+            return Fraction((self.numerator * fraction.denominator) + (self.denominator * fraction.numerator), (self.denominator * fraction.denominator))
         }
     }
+    
+    func subtraction(fraction: Fraction?) -> Fraction? {
+        guard let fraction = fraction else { return nil }
+        if self.denominator == fraction.denominator {
+            return Fraction(self.numerator - fraction.numerator, self.denominator)
+        } else {
+            return Fraction((self.numerator * fraction.denominator) - (self.denominator * fraction.numerator), (self.denominator * fraction.denominator))
+        }
+    }
+    
+    func multiplication(fraction: Fraction?) -> Fraction? {
+        guard let fraction = fraction else { return nil }
+        return Fraction(self.numerator * fraction.numerator, self.denominator * fraction.denominator)
+    }
+    
+    func division(fraction: Fraction?) -> Fraction? {
+        guard let fraction = fraction else { return nil }
+        return Fraction(self.numerator * fraction.denominator, self.denominator * fraction.numerator)
+    }
 }
+
+let firstFraction = Fraction(5, 2)
+let secondFraction = Fraction(4, 2)
+
+if let f = firstFraction, let s = secondFraction {
+    if let plusResult = f.summation(fraction: s) {
+        print("Summation: \(plusResult.numerator), \(plusResult.denominator)")
+    }
+}
+
+if let f = firstFraction, let s = secondFraction {
+    if let plusResult = f.subtraction(fraction: s) {
+        print("Subtraction: \(plusResult.numerator), \(plusResult.denominator)")
+    }
+}
+
+if let f = firstFraction, let s = secondFraction {
+    if let plusResult = f.multiplication(fraction: s) {
+        print("Multiplication \(plusResult.numerator), \(plusResult.denominator)")
+    }
+}
+
+if let f = firstFraction, let s = secondFraction {
+    if let plusResult = f.division(fraction: s) {
+        print("Division: \(plusResult.numerator), \(plusResult.denominator)")
+    }
+}
+
+
+
+
