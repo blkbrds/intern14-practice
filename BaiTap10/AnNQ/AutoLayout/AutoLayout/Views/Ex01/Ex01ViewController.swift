d74f7ef0b4cbac7dc70b3325c34fd1dc6e91cf12
at-annguyen <an.nguyen@asiantech.vn>
2019-10-04 11:37:51 +0700

    Bai 1-6

---
+++ BaiTap10/AnNQ/AutoLayout/AutoLayout/Views/Ex01/Ex01ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Ex01ViewController.swift
+//  AutoLayout
+//
+//  Created by MBA0217 on 10/2/19.
+//  Copyright © 2019 MBA0217. All rights reserved.
+//
+
+import UIKit
+
+class Ex01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Example 1"
+    }
+
+
+}
---
+++ BaiTap10/AnNQ/AutoLayout/AutoLayout/Views/Ex04/Ex04ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Ex04ViewController.swift
+//  AutoLayout
+//
+//  Created by MBA0217 on 10/2/19.
+//  Copyright © 2019 MBA0217. All rights reserved.
+//
+
+import UIKit
+
+class Ex04ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Example 4"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
