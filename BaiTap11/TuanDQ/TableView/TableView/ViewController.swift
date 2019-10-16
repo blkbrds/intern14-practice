124f0c6d433939e0cb365ebfd69751eaad47f941
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-10-16 08:51:52 +0700

    Initial project

---
+++ BaiTap11/TuanDQ/TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by MBA0051 on 10/16/19.
+//  Copyright © 2019 MBA0051. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
