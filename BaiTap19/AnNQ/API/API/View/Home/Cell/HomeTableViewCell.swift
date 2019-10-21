758dee43cfb9ef36fbe255c10dc19e307671e5af
at-annguyen <an.nguyen@asiantech.vn>
2019-10-21 10:54:40 +0700

    MVVM Struct

---
+++ BaiTap19/AnNQ/API/API/View/Home/Cell/HomeTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  HomeTableViewCell.swift
+//  API
+//
+//  Created by MBA0217 on 10/21/19.
+//  Copyright © 2019 MBA0217. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BaiTap19/AnNQ/API/API/View/Home/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.swift
+//  API
+//
+//  Created by MBA0217 on 10/21/19.
+//  Copyright © 2019 MBA0217. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Configuration UI
+    private func configureUI() {
+        title = "Home"
+        TableView.loadNibName(nibName: "HomeTableViewCell", tableView: tableView)
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
