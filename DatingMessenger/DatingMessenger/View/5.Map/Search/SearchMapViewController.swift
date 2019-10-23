4788bcc9f6a043bda5033954f3ee616784527372
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-10-23 16:54:31 +0700

    Add map search initial.

---
+++ DatingMessenger/DatingMessenger/View/5.Map/Search/SearchMapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchMapViewController.swift
+//  DatingMessenger
+//
+//  Created by MBA0051 on 10/23/19.
+//  Copyright © 2019 MBA0051. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SearchMapViewController: UIViewController {
+
+    @IBOutlet weak var locationSearchBar: UISearchBar!
+    @IBOutlet weak var localtionMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
